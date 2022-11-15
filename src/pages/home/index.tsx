cfd68944092dbef20516c35486c48d5fecf5c37c
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-11-15 11:20:48 -0500

    pagination

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,12 @@
-import { Button, Container, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Grid,
+  Pagination,
+  Stack,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 
 import { characters } from "../../api/characters";
@@ -6,18 +14,32 @@ import { CardComponent, HeaderComponent } from "../../components";
 import { TypeCharacter } from "./interface/character.interface";
 
 export const HomePage: React.FC<{}> = () => {
-  const [allCharacters, setAllCharacters] = useState<TypeCharacter[]>([]);
+  const [allCharacters, setAllCharacters] = useState<TypeCharacter[] | null>(
+    null
+  );
+  const [count, setCount] = useState<number>(1);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    setLoading(true);
     characters
-      .getAll({ page: 1 })
+      .getAll({ page })
       .then((res) => {
+        setCount(res.data.info.pages);
         setAllCharacters(res.data.results);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
       })
       .catch((err) => {
         console.error(err);
       });
-  }, []);
+  }, [page]);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   return (
     <Container sx={{ mt: 9 }} maxWidth="xl">
@@ -30,25 +52,45 @@ export const HomePage: React.FC<{}> = () => {
           </Button>
         }
       />
-      <div>
-        {allCharacters!.length !== 0 ? (
-          <Grid container spacing={2} direction="row">
-            {allCharacters.map((character) => (
-              <Grid item xs={3}>
-                <CardComponent
-                  key={character.id}
-                  image={character.image}
-                  name={character.name}
-                  species={character.species}
-                  status={character.status}
-                />
+      {loading ? (
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <div>
+            {allCharacters!.length !== 0 ? (
+              <Grid container spacing={2} direction="row">
+                {allCharacters!.map((character) => (
+                  <Grid item xs={3} key={character.id}>
+                    <CardComponent
+                      image={character.image}
+                      name={character.name}
+                      species={character.species}
+                      status={character.status}
+                    />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        ) : (
-          <h1>Cargando...</h1>
-        )}
-      </div>
+            ) : (
+              <h1>No hay personajes</h1>
+            )}
+          </div>
+          <Box
+            sx={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <Pagination
+              variant="outlined"
+              color="primary"
+              count={count}
+              page={page}
+              onChange={handleChange}
+              sx={{ my: 3 }}
+              size="large"
+            />
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
